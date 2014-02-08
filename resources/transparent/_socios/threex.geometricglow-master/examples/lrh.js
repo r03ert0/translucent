b9ac504d5c91eb3d4a6bb698c76737a2de93c175
roberto <roberto@golgi.local>
2014-02-08 17:37:51 +0100

    translucent: 1st commit
    A translucent brain to display 3d data

---
+++ resources/transparent/_socios/threex.geometricglow-master/examples/lrh.js
@@ -0,0 +1,23 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "lrh.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 5124,
+        "faces"         : 10240,
+        "normals"       : 0,
+        "uvs"           : 0,
+        "materials"     : 0
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "buffers": "lrh.bin"
+
+}
---
+++ resources/transparent/lrh3.js
@@ -0,0 +1,23 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "lrh3.pial.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 1284,
+        "faces"         : 2560,
+        "normals"       : 0,
+        "uvs"           : 0,
+        "materials"     : 0
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "buffers": "lrh3.bin"
+
+}
